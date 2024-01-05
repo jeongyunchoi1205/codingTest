da801d88777db2a617a03a1a45863a37711cedc5
jeongyunchoi <124752847+jeongyunchoi1205@users.noreply.github.com>
2024-01-05 20:13:39 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i=0; i<=n; i++){
+            if(i%2!=0){
+                answer.add(i);
+            }
+        }
+        
+        return answer;
+    }
+}
