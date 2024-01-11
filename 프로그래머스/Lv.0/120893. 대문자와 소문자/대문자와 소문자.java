ff0ea5974c37b3ed9a7cd9dc5e42029d2545600e
jeongyunchoi <124752847+jeongyunchoi1205@users.noreply.github.com>
2024-01-11 17:59:30 +0900

    [level Lv.0] Title: 대문자와 소문자, Time: 3.23 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++){
+            if(my_string.charAt(i)>=65 && my_string.charAt(i)<=90){
+                answer += String.valueOf(my_string.charAt(i)).toLowerCase();
+            }else{
+                answer += String.valueOf(my_string.charAt(i)).toUpperCase();
+            }
+        }
+        return answer;
+    }
+}
